3e01a7caa58a1571d19ad890ac5fba5ea3657751
JustSecret123 <ibrahimserouis99@hotmail.com>
2026-02-05 16:09:56 +0100

    FIXED : breaks in image generation test scripts

--- experiments/test scripts/test_gen_images.py
+++ experiments/test scripts/test_gen_images.py
@@ -113,10 +113,10 @@ def generate_image_animagine(code_file,GPU="0"):
                     # Get the prompt infos
                     prompt_number, prompt_level, synthetic_prompts = extract_prompt_info(prompt)
                     # Loop through the synthetic prompts
-                    for i,gen_prompt in enumerate(synthetic_prompts):
+                    for j,gen_prompt in enumerate(synthetic_prompts):
                         # Set the output file name
                         output_file_name = set_output_name(model_name, skill_code, 
-                                                           prompt_level,prompt_number,i, output_dir_name, robust)
+                                                           prompt_level,prompt_number,j,output_dir_name, robust)
                         # Check if the file doesn't exist
                         if output_file_name not in output_files:
                             # Display the output file name
@@ -134,8 +134,6 @@ def generate_image_animagine(code_file,GPU="0"):
                             image.save(output_file_name+".png")
                             # Display success message
                             print(f"\nImage generated for : {output_file_name}. Prompt: {gen_prompt}")
-                            break
-                        break
         # Free memory
         del image, pipe
         # Collect garbage
@@ -157,11 +155,13 @@ def generate_image_stable_diffusion(code_file,GPU="0"):
     json_dir = Path("../../outputs/prompts")
     # Get the list of json files
     json_files = [p for p in json_dir.glob("*") if p.is_file()]
+    json_files.sort()
     # Set the image output directory
     output_dir_name = "../../data/images_refactored/stable_diffusion"
     output_dir = Path(output_dir_name)
     # Get the list of files to generate
     output_files = [p for p in output_dir.glob("*") if p.is_file()]
+    output_files.sort()
 
     try: 
         # Import relevant packages
@@ -178,6 +178,103 @@ def generate_image_stable_diffusion(code_file,GPU="0"):
         model_name = "stable_diffusion"
         # Display a message
         print("\nGenerating images with stable diffusion.\n")
+        # Loop through the files
+        for i,json_file in enumerate(json_files):
+            print(json_file)
+            # Extract the skill code
+            skill_code, robust = extract_code_and_robust(code_file,os.path.basename(json_file))
+            # Initialize the collection
+            elements = dict()
+            # Open the file
+            with open(json_file, "rb") as file:
+                # Load the elements into a dict
+                elements = json.load(file)
+                # Loop through the prompts
+                for prompt in elements["prompts"]:
+                    # Get the prompt infos
+                    prompt_number, prompt_level, synthetic_prompts = extract_prompt_info(prompt)
+                    # Loop through the synthetic prompts
+                    for j,gen_prompt in enumerate(synthetic_prompts):
+                        # Set the output file name
+                        output_file_name = set_output_name(model_name=model_name, 
+                                                           skill_code=skill_code, 
+                                                           prompt_level=prompt_level,
+                                                           prompt_number=prompt_number,
+                                                           synthetic_prompt_number=j,
+                                                           output_folder_name=output_dir_name, 
+                                                           robust=robust)
+                        # Check if the file doesn't exist
+                        if output_file_name not in output_files:
+                            # Display the output file name
+                            print(f"\nOutput file name: {output_file_name}")
+                            # Run inference ; generate the image
+                            with torch.no_grad():
+                                # Adding support for long prompts
+                                (prompt_embeds, 
+                                 prompt_neg_embeds,
+                                 pooled_prompt_embeds,
+                                 negative_pooled_prompt_embeds) = get_weighted_text_embeddings_sdxl(
+                                     pipe,
+                                     prompt = gen_prompt,
+                                )
+                                image = pipe(
+                                    prompt_embeds=prompt_embeds,
+                                    negative_prompt_embeds=prompt_neg_embeds,
+                                    pooled_prompt_embeds=pooled_prompt_embeds,
+                                    negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
+                                    width=1024,
+                                    height=1024, 
+                                    guidance_scale=5, 
+                                    num_inference_steps=50, 
+                                    generator=torch.Generator(f"cuda:{GPU}").manual_seed(42) #Setting the seed to be more deterministic
+                                ).images[0]
+                                # Save image
+                                image.save(output_file_name+".png")
+                            # Display success message
+                            print(f"\nImage generated for : {output_file_name}. Prompt: {gen_prompt}")
+        # Free memory
+        del image, pipe
+        # Collect garbage
+        gc.collect()
+        # Empty cuda cache
+        torch.cuda.empty_cache()
+        # Collect garbage
+        torch.cuda.ipc_collect()
+        
+        return 1
+    except Exception:
+        # Display exception
+        traceback.print_exc()
+
+
+def generate_z_image(code_file,GPU="0"):
+    """
+    """
+    # Set the json outputs directory (.json)
+    json_dir = Path("../../outputs/prompts")
+    # Get the list of json files
+    json_files = [p for p in json_dir.glob("*") if p.is_file()]
+    # Set the image output directory
+    output_dir_name = "../../data/images_refactored/z_image_turbo"
+    output_dir = Path(output_dir_name)
+    # Get the list of files to generate
+    output_files = [p for p in output_dir.glob("*") if p.is_file()]
+
+    try: 
+        # Import relevant packages
+        from diffusers import ZImagePipeline
+        from sd_embed.embedding_funcs import get_weighted_text_embeddings_sdxl
+        # Setup the pipe
+        pipe = ZImagePipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", 
+                                                 torch_dtype=torch.float16, 
+                                                 use_safetensors=True,
+                                                 variant="fp16").to(f"cuda:{GPU}")
+        
+        
+        # Set the elements 
+        model_name = "z_image_turbo"
+        # Display a message
+        print("\nGenerating images with z_image_turbo.\n")
         # Loop through the files
         for i,json_file in enumerate(json_files):
             print(json_file)
@@ -227,8 +324,6 @@ def generate_image_stable_diffusion(code_file,GPU="0"):
                                 image.save(output_file_name+".png")
                             # Display success message
                             print(f"\nImage generated for : {output_file_name}. Prompt: {gen_prompt}")
-                            break
-                        break
         # Free memory
         del image, pipe
         # Collect garbage
