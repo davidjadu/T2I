c68567a969218983bc396ed91b2b69e4f1444e17
JustSecret123 <ibrahimserouis99@hotmail.com>
2026-02-06 10:51:13 +0100

    ADDED : Z-image inference script

--- experiments/test scripts/test_gen_images.py
+++ experiments/test scripts/test_gen_images.py
@@ -55,7 +55,8 @@ def extract_prompt_info(prompt:dict):
     return prompt_number,prompt_level,synthetic_prompts
 
 def set_output_name(model_name:str,skill_code,prompt_level:str,prompt_number:str, synthetic_prompt_number:int, output_folder_name:str, robust:bool):
-
+    """
+    """
 
     # Initialize the variable
     output_file_name = ""
@@ -150,7 +151,15 @@ def generate_image_animagine(code_file,GPU="0"):
 
 def generate_image_stable_diffusion(code_file,GPU="0"):
     """
+    Generates images using the Stable Diffusion model. 
+    Doesn't generate if the output file already exists.
+    Inference parameters are the same for most models : 
+    - Inference steps : 50
+    - Guidance scale : 5.0
+    - Width and Height : 1024x1024
+    We use textual inversion from SD embed for the model to support "longer" prompts. 
     """
+
     # Set the json outputs directory (.json)
     json_dir = Path("../../outputs/prompts")
     # Get the list of json files
@@ -172,7 +181,6 @@ def generate_image_stable_diffusion(code_file,GPU="0"):
                                                  torch_dtype=torch.float16, 
                                                  use_safetensors=True,
                                                  variant="fp16").to(f"cuda:{GPU}")
-        
         
         # Set the elements 
         model_name = "stable_diffusion"
@@ -246,30 +254,36 @@ def generate_image_stable_diffusion(code_file,GPU="0"):
         # Display exception
         traceback.print_exc()
 
+def generate_z_image_turbo(code_file,GPU="0"):
+    """
+    Generates images using the Z-image Turbo model. 
+    Doesn't generate if the output file already exists.
+    Inference parameters are the same for most models : 
+    - Inference steps : 50
+    - Guidance scale : 5.0
+    - Width and Height : 1024x1024
+    We use textual inversion from SD embed for the model to support "longer" prompts. 
+    """
 
-def generate_z_image(code_file,GPU="0"):
-    """
-    """
     # Set the json outputs directory (.json)
     json_dir = Path("../../outputs/prompts")
     # Get the list of json files
     json_files = [p for p in json_dir.glob("*") if p.is_file()]
+    json_files.sort()
     # Set the image output directory
     output_dir_name = "../../data/images_refactored/z_image_turbo"
     output_dir = Path(output_dir_name)
     # Get the list of files to generate
     output_files = [p._str for p in output_dir.glob("*") if p.is_file()]
+    output_files.sort()
 
     try: 
         # Import relevant packages
-        from diffusers import ZImagePipeline
-        from sd_embed.embedding_funcs import get_weighted_text_embeddings_sdxl
+        from diffusers import DiffusionPipeline
         # Setup the pipe
-        pipe = ZImagePipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", 
-                                                 torch_dtype=torch.float16, 
-                                                 use_safetensors=True,
-                                                 variant="fp16").to(f"cuda:{GPU}")
-        
+        pipe = DiffusionPipeline.from_pretrained("Tongyi-MAI/Z-Image-Turbo", 
+                                                 torch_dtype=torch.bfloat16, 
+                                                 use_safetensors=True).to(f"cuda:{GPU}")
         
         # Set the elements 
         model_name = "z_image_turbo"
@@ -293,31 +307,25 @@ def generate_z_image(code_file,GPU="0"):
                     # Loop through the synthetic prompts
                     for j,gen_prompt in enumerate(synthetic_prompts):
                         # Set the output file name
-                        output_file_name = set_output_name(model_name, skill_code, 
-                                                           prompt_level,prompt_number,j, output_dir_name, robust)
+                        output_file_name = set_output_name(model_name=model_name, 
+                                                           skill_code=skill_code, 
+                                                           prompt_level=prompt_level,
+                                                           prompt_number=prompt_number,
+                                                           synthetic_prompt_number=j,
+                                                           output_folder_name=output_dir_name, 
+                                                           robust=robust)
                         # Check if the file doesn't exist
                         if output_file_name+".png" not in output_files:
                             # Display the output file name
                             print(f"\nOutput file name: {output_file_name}")
                             # Run inference ; generate the image
                             with torch.no_grad():
-                                # Adding support for long prompts
-                                (prompt_embeds, 
-                                 prompt_neg_embeds,
-                                 pooled_prompt_embeds,
-                                 negative_pooled_prompt_embeds) = get_weighted_text_embeddings_sdxl(
-                                     pipe,
-                                     prompt = gen_prompt,
-                                )
                                 image = pipe(
-                                    prompt_embeds=prompt_embeds,
-                                    negative_prompt_embeds=prompt_neg_embeds,
-                                    pooled_prompt_embeds=pooled_prompt_embeds,
-                                    negative_pooled_prompt_embeds=negative_pooled_prompt_embeds,
+                                    prompt=gen_prompt,
                                     width=1024,
                                     height=1024, 
-                                    guidance_scale=5, 
-                                    num_inference_steps=50, 
+                                    guidance_scale=0.0, 
+                                    num_inference_steps=9, 
                                     generator=torch.Generator(f"cuda:{GPU}").manual_seed(42) #Setting the seed to be more deterministic
                                 ).images[0]
                                 # Save image
@@ -356,8 +364,8 @@ def initialize_parser():
                                             "runway", 
                                             "stable_cascade", 
                                             "stable_diffusion", 
-                                            "z_image"], default="stable_diffusion"),
-    
+                                            "z_image"], default="z_image")
+
 
     return parser
 
@@ -387,6 +395,9 @@ def main():
     elif MODEL=="stable_diffusion":
         # Generate images with stable diffusion
         generate_image_stable_diffusion(code_file,GPU)
+    elif MODEL=="z_image":
+        # Generat images with z-image turbo
+        generate_z_image_turbo(code_file,GPU)
 
 
 
